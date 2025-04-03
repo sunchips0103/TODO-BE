21f3177c1606fd68301bafd0132390eec9ebfe6d
Taeyang Guk <rnrxodid@naver.com>
2025-04-03 12:38:21 +0900

    first commit

---
+++ routes/task.api.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+router.post("/",(req,res)=>{
+    res.send("create task");
+});
+router.get("/",(req,res)=>{
+    res.send("get tasks");
+});
+router.put("/:id",(req,res)=>{
+    res.send("update task");
+});
+router.delete("/:id",(req,res)=>{
+    res.send("delete tasks");
+});
+
+module.exports = router;
