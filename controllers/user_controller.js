7878093b251584a6fa9c81dc39f37d19bb715464
Taeyang Guk <rnrxodid@naver.com>
2025-04-10 01:50:51 +0900

    createLogin

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -26,4 +26,23 @@ userController.createUser = async (req,res)=>{
     }
 };
 
+userController.loginWithEmail=async(req,res)=>{
+    try{
+        const{email,password} =req.body;
+        const user = await User.findOne({email}, "-createdAt -updatedAt -__v");
+        if(user){
+            const isMath= bcrypt.compareSync(password, user.password); 
+            if(isMath){
+                const token = user.generateToken();
+                return res.status(200).json({status:"success", user, token});
+            }
+        }
+        throw new Error("아이디 또는 비밀번호가 일치하지 않습니다");
+    }
+    catch(error){
+        res.status(400).json({status :"fail",error});
+    }
+};
+
 module.exports = userController;
+
--- routes/user.api.js
+++ routes/user.api.js
@@ -4,5 +4,5 @@ const userController = require('../controllers/user_controller');
 
 //1.회원가입 endpoint
 router.post("/", userController.createUser);
-
+router.post("/login", userController.loginWithEmail);//이메일, 패스워드 읽어오려고 post사용 //get은 req.body 사용 x
 module.exports=router;
